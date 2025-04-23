10d64870e16a7429cdc4483c39741f97b13cdb21
bbfox0703 <101542129+bbfox0703@users.noreply.github.com>
2025-04-23 18:59:13 +0800

    Update update.sh

--- cronjob/update.sh
+++ cronjob/update.sh
@@ -3,6 +3,6 @@
 # 跑 Flask 更新
 curl http://localhost:3000/update
 
-# 保存快照（只留最近 24 份）
+# 保存快照（只留最近 30 份）
 cp database/friends.json backups/friends_$(date +'%Y%m%d_%H%M%S').json
-ls -1t backups/friends_*.json | tail -n +25 | xargs rm -f
+ls -1t backups/friends_*.json | tail -n +31 | xargs rm -f

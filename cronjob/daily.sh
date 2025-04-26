24e8b87d38fd9441ec3449dfdb04b7b7e83dd6e7
bbfox0703 <101542129+bbfox0703@users.noreply.github.com>
2025-04-26 22:26:08 +0800

    Update daily.sh

--- cronjob/daily.sh
+++ cronjob/daily.sh
@@ -9,12 +9,17 @@ export PYTHONPATH="/app"
 # 切換到專案根目錄
 cd /app
 
-# 每天凌晨存長期備份
+# 每天資料備份
 cp /app/database/friends.json /app/backups/daily_friends_$(date +'%Y%m%d').json
 cp /app/database/friend_changes.json /app/backups/daily_friend_changes_$(date +'%Y%m%d').json
 cp /app/database/name_history.json /app/backups/daily_name_history_$(date +'%Y%m%d').json
 cp /app/database/game_titles.json /app/backups/daily_game_titles_$(date +'%Y%m%d').json
 cp /app/database/level_history.json /app/backups/daily_level_history_$(date +'%Y%m%d').json
+cp /app/database/achievement_trend.json /app/backups/daily_achievement_trend_$(date +'%Y%m%d').json
+cp /app/database/playtime_trend.json /app/backups/daily_playtime_trend_$(date +'%Y%m%d').json
+
+
+achievement_trend.json
 
 # 刪除 30 天以前的 daily 備份
 find /app/backups/ -name "daily_*.json" -mtime +30 -delete

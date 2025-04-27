3ca53fc23fb69ff3538a23e092e590701370a10b
bbfox0703 <101542129+bbfox0703@users.noreply.github.com>
2025-04-27 21:39:05 +0800

    Update daily.sh

--- cronjob/daily.sh
+++ cronjob/daily.sh
@@ -10,10 +10,6 @@ cd /app
 cp /app/database/friends.json /app/backups/daily_friends_$(date +'%Y%m%d').json
 cp /app/database/friend_changes.json /app/backups/daily_friend_changes_$(date +'%Y%m%d').json
 cp /app/database/name_history.json /app/backups/daily_name_history_$(date +'%Y%m%d').json
-cp /app/database/game_titles.json /app/backups/daily_game_titles_$(date +'%Y%m%d').json
-cp /app/database/level_history.json /app/backups/daily_level_history_$(date +'%Y%m%d').json
-cp /app/database/achievement_trend.json /app/backups/daily_achievement_trend_$(date +'%Y%m%d').json
-cp /app/database/playtime_trend.json /app/backups/daily_playtime_trend_$(date +'%Y%m%d').json
 cp /app/database/games_total_history.json /app/backups/daily_games_total_history_$(date +'%Y%m%d').json
 
 # 刪除 30 天以前的 daily 備份

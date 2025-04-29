b3296461258602a055c8f39d6021a494b36f34fe
bbfox0703 <101542129+bbfox0703@users.noreply.github.com>
2025-04-29 22:38:30 +0800

    Update steam_api.py

--- utils/steam_api.py
+++ utils/steam_api.py
@@ -33,6 +33,7 @@ def log(msg):
     timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
     full_msg = f"[{timestamp}] {msg}"
     print(full_msg)
+    print(LOG_FILE)
     os.makedirs(LOG_DIR, exist_ok=True)
     with open(LOG_FILE, "a", encoding="utf-8") as f:
         f.write(full_msg + "\n")

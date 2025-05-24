09c7aab650167e5cc4a1a0bb61a779e84d5d746f
bbfox0703 <101542129+bbfox0703@users.noreply.github.com>
2025-05-24 13:40:54 +0800

    Update record_games_total.sh

--- cronjob/record_games_total.sh
+++ cronjob/record_games_total.sh
@@ -5,5 +5,5 @@
   echo "[$(date '+%Y-%m-%d %H:%M:%S')] 🕒 record_games_total.sh started" >> /var/log/cron_exec.log
   export PYTHONPATH="/app"
   cd /app
-  /usr/local/bin/python3 -m utils.record_games_total >> /var/log/cron_exec.log 2 /var/log/record_games_total_err.log
+  /usr/local/bin/python3 -m utils.record_games_total >> /var/log/cron_exec.log 2>> /var/log/record_games_total_err.log
 ) 9>/tmp/record_games_total.lock
